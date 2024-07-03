24058c0599e16bef25bea94f6eba7d6519d501b1
Guilherme <111542995+UxieGu1@users.noreply.github.com>
2024-07-03 18:35:28 -0300

    Update Ex008.java

--- While/Ex008.java
+++ While/Ex008.java
@@ -1,6 +1,6 @@
 //Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha
-//        igual ao nome do usuário, mostrando uma mensagem de erro e voltando a pedir as
-//        informações. O login é terra e a senha é plana
+//igual ao nome do usuário, mostrando uma mensagem de erro e voltando a pedir as
+//informações. O login é terra e a senha é plana
 
 package loopwhile;
 import java.util.Scanner;
