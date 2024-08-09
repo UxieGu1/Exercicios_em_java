be37669d47411e6755338001ac9711d7683fa886
Guilherme <111542995+UxieGu1@users.noreply.github.com>
2024-08-09 15:37:43 -0300

    Create fibonacci.java

---
+++ Recursividade/fibonacci.java
@@ -0,0 +1,17 @@
+public class Fibonacci {
+  public static int fibonacci(int n){
+    if(n == 0){
+      return 0;
+    }
+    else if(n == 1){
+      return 1;
+    }else{
+      return fibonacci(n-1) + fibonacci(n-2);
+    }
+  }
+  public static void main(String[] args) {
+    int resultado = fibonacci(15);
+    System.out.println(resultado);
+  }
+
+}
