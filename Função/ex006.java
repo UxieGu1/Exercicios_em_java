eb336793f7dc909812db5a682cf04985140f9723
Guilherme <111542995+UxieGu1@users.noreply.github.com>
2025-02-25 11:41:18 -0300

    Update ex006.java

--- Função/ex006.java
+++ Função/ex006.java
@@ -3,10 +3,6 @@
 
 package Funcoes;
 
-//Crie uma função que receba como entrada uma string e retorne “É palíndromo” se a string for
-//um palíndromo e “Não é palíndromo” caso contrário.
-
-
 import java.util.Scanner;
 
 public class ex006 {
